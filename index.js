0bb3ae1acfaeca45d19a34914369f2569b6f76a2
motswanax <motswanax@users.noreply.github.com>
2022-04-12 15:39:56 +0200

    Replaced callbacks with promises.

--- index.js
+++ index.js
@@ -15,18 +15,33 @@ function displayCommits(commits) {
 }
 
 // Promises
+// A promise holds the eventual result of an asynchronous operation. It promises to give you something, even if it's an error!
 // Async/await
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({ id: id, gitHubUsername: "motswanax" });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work.
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({ id: id, gitHubUsername: "motswanax" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("Calling GitHub API...");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
 }
---
+++ promise.js
@@ -0,0 +1,12 @@
+const p = new Promise((resolve, reject) => {
+  // Kick off some async work.
+  setTimeout(() => {
+    //resolve(1);
+    reject(new Error("message"));
+  }, 2000);
+});
+
+// Consume the promise.
+p.then((result) => console.log("Result", result)).catch((err) =>
+  console.log("Error", err.message)
+);
