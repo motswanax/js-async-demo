3ae77c454ef3f80298386f7ce33b196b94daba2e
motswanax <motswanax@users.noreply.github.com>
2022-04-13 23:42:09 +0200

    Async and await in JS

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 console.log("Before");
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
-console.log("After");
 
 // Promises
 // A promise holds the eventual result of an asynchronous operation. It promises to give you something, even if it's an error!
+/* getUser(1)
+  .then((user) => getRepositories(user.gitHubUsername))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log("Commits", commits))
+  .catch((err) => console.log("Error", err.message)); */
+
 // Async/await
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+}
+
+displayCommits();
+
+console.log("After");
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
@@ -24,7 +38,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      //resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Could not get the repos."));
     }, 2000);
   });
 }
